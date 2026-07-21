76c0616661cd24f44dc74b671df2cc2b5274ded1
px41529-maker <px41529@gmail.com>
2026-07-21 20:51:04 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -6,22 +6,20 @@ module "eks" {
   cluster_name    = "demo-cluster"
   cluster_version = "1.34"
 
-  /*cluster_endpoint = true*/
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 
   vpc_id     = data.aws_vpc.default.id
   subnet_ids = data.aws_subnets.default.ids
 
   eks_managed_node_groups = {
     default = {
-
       instance_types = [var.instance_type]
-
       ami_type       = "AL2023_x86_64_STANDARD"
 
-      desired_size   = var.desired_size
-      min_size       = var.min_size
-      max_size       = var.max_size
-
+      desired_size = var.desired_size
+      min_size     = var.min_size
+      max_size     = var.max_size
     }
   }
 
